bbcbd75416ae72c37a3c7855150bec5851d4dd09
Jay Sargent <sargentjay11@gmail.com>
2021-12-13 22:19:09 -0700

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './global.scss';
+
+export const Main = () => <h1 className="main">Hello React</h1>;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.scss/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+		}),
+	],
+	devServer: {
+		static: path.resolve(__dirname, 'dist'),
+	},
+};
